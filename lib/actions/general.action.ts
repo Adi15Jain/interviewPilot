3b93d61e76aaa8411a264e0c617eb841f8490ece
Adi Jain <adi1510jain@gmail.com>
2026-02-06 01:31:19 +0530

    Model Changes

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -13,7 +13,7 @@ export async function createFeedback(params: CreateFeedbackParams) {
         const formattedTranscript = transcript
             .map(
                 (sentence: { role: string; content: string }) =>
-                    `- ${sentence.role}: ${sentence.content}\n`
+                    `- ${sentence.role}: ${sentence.content}\n`,
             )
             .join("");
 
@@ -83,7 +83,7 @@ export async function getInterviewById(id: string): Promise<Interview | null> {
 }
 
 export async function getFeedbackByInterviewId(
-    params: GetFeedbackByInterviewIdParams
+    params: GetFeedbackByInterviewIdParams,
 ): Promise<Feedback | null> {
     const { interviewId, userId } = params;
 
@@ -111,7 +111,7 @@ export async function getFeedbackByInterviewId(
 }
 
 export async function getLatestInterviews(
-    params: GetLatestInterviewsParams
+    params: GetLatestInterviewsParams,
 ): Promise<Interview[] | null> {
     const { userId, limit = 20 } = params;
 
@@ -140,7 +140,7 @@ export async function getLatestInterviews(
 }
 
 export async function getInterviewsByUserId(
-    userId: string
+    userId: string,
 ): Promise<Interview[] | null> {
     try {
         const interviews = await prisma.interview.findMany({
